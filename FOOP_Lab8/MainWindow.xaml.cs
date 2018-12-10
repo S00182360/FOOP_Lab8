8bd02fcb321e3e726c7370e797374c21bf60786d
Samuel Colville <S00182360@mail.itsligo.ie>
2018-12-10 21:33:37 +0000

    Game works.

--- FOOP_Lab8/MainWindow.xaml.cs
+++ FOOP_Lab8/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace FOOP_Lab8
         public int Number1 { get; set; }
         public int Number2 { get; set; }
 
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,9 +32,41 @@ namespace FOOP_Lab8
 
         private void Btn_StartGame_Click(object sender, RoutedEventArgs e)
         {
-            Number1 = r.Next(0, 20);
-            Number2 = r.Next(0, 20);
-            tbk_Initial.Text = string.Format("Number: {0}", Number1);
+            Number1 = r.Next(1, 21);
+            Number2 = r.Next(1, 21);
+            tbk_Initial.Text = "Number : " + Number1.ToString();
+            tbk_Number2.Text = Number2.ToString();
+            tbk_WinLoose.Text = "";
+        }
+
+        private void Btn_Guess_Click(object sender, RoutedEventArgs e)
+        {
+            bool isBigger = false;
+            if (Number1 <= Number2)
+                isBigger = true;
+            else
+                isBigger = false;
+            bool selectedBigger = rad_Bigger.IsChecked.Equals(true);
+            bool selectedSmaller = rad_Small.IsChecked.Equals(true);
+            if ((isBigger && selectedBigger) || (selectedSmaller && !isBigger))
+                tbk_WinLoose.Text = "You win!";
+            else
+                tbk_WinLoose.Text = "You Loose";
+            Number1 = r.Next(1, 21);
+            Number2 = r.Next(1, 21);
+            tbk_Initial.Text = Number1.ToString();
+            //This line of code was used in development to check 
+            //above logic worked.
+            tbk_Number2.Text = Number2.ToString();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbk_Initial.Text = "Press 'Start Game' to begin.";
+            tbk_WinLoose.Text = "";
+            tbk_Instruct.Text = "Is the next number bigger or smaller?";
         }
     }
 }
